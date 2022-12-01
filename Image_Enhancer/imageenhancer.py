c949c9fef94dcbc3f1a8c289e10c2f53694eecc1
KyleLino <kyle_lino@dlsu.edu.ph>
2022-12-01 18:46:27 +0800

    added folders for clean look

--- Image_Enhancer/imageenhancer.py
+++ Image_Enhancer/imageenhancer.py
@@ -1,19 +1,19 @@
 from PIL import Image, ImageEnhance
 
 #read the image
-im = Image.open("input_images/image_1.png")
+im = Image.open("Image_Enhancer/input_images/image_1.png")
 
 #image brightness enhancer
 enhancer = ImageEnhance.Brightness(im)
 
 factor = 1 #gives original image
 im_output = enhancer.enhance(factor)
-im_output.save('output_images/original-image.png')
+im_output.save('Image_Enhancer/output_images/original-image.png')
 
 factor = 0.5 #darkens the image
 im_output = enhancer.enhance(factor)
-im_output.save('output_images/darkened-image.png')
+im_output.save('Image_Enhancer/output_images/darkened-image.png')
 
 factor = 1.5 #brightens the image
 im_output = enhancer.enhance(factor)
-im_output.save('output_images/brightened-image.png')
+im_output.save('Image_Enhancer/output_images/brightened-image.png')
