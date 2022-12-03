e9ddb207a6edf9c48fdb2087b61583115cac0593
KyleLino <kyle_lino@dlsu.edu.ph>
2022-12-03 19:54:52 +0800

    added line to duplicate 3 img files to 900

--- Image_Enhancer/imageenhancer.py
+++ Image_Enhancer/imageenhancer.py
@@ -4,16 +4,21 @@ import glob
 #all image directories
 images = glob.glob("Image_Enhancer/input_images/*")
 
+
+#creates 900 image files from the 3 original images files 
+#images = images * 300
+
 #b = int(input('Enter brightness value:'))
 #s = int(input('Enter sharpness value:'))
 #c = int(input('Enter contrast value:'))
 
-#print(images)
+print(images)
 
 #FACTORS
 brightness_factor = 1
 sharpness_factor = 1
 contrast_factor = 1
+#number of images saved
 x = 0
 
 for image_file in images:
@@ -26,9 +31,9 @@ for image_file in images:
     image = sharpness_enhancer.enhance(sharpness_factor)
     contrast_enhancer = ImageEnhance.Contrast(image)
     image = contrast_enhancer.enhance(contrast_factor) 
-    x += 1 #counter
 
     #saves enghanced images into png
+    x += 1 #counter
     image.save('Image_Enhancer/output_images/enhanced_image_'+ str(x) +'.png')
 
 
